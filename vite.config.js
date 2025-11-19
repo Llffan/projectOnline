e6a3076984f8d0ab98959423758339b462f47641
赖凡 <3475662079@qq.com>
2025-11-19 23:02:28 +0800

    change vercel.json

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+  plugins: [vue()],
+  build: {
+    outDir: 'dist', // 必须与 vercel.json 中的 distDir 一致
+    assetsDir: 'assets', // 静态资源目录
+    sourcemap: false, // 生产环境关闭 sourcemap
+    rollupOptions: {
+      output: {
+        // 静态资源分类打包
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]'
+      }
+    }
   },
+  server: {
+    open: true // 开发环境自动打开浏览器
+  }
 })
